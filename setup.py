3de7d3a1d21bfd0e22de03ce3848c51d9379cda4
Alexandru Mariuti <alex@mariuti.com>
2024-09-24 18:08:16 +0100

    fix: move code into module

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="prompt_parser",
-    version="0.1.0",
+    version="0.1.1",
     author="Alexandru Mariuti",
     author_email="alex@mariuti.com",
     description="A simple Python library for parsing LLM prompts.",
